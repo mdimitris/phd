d43641dca1e05041e45884117282caa7cf3205c6
Dimitris Markopoulos <m_dimitris@hotmail.com>
2025-09-21 22:08:08 +0300

    first interpolation

--- newapp/lstm-final2.py
+++ newapp/lstm-final2.py
@@ -45,9 +45,13 @@ dtypes = {
 #     '/root/scripts/new_data/24hours/vitals_24_hours_final.csv',
 #     sep='|',
 #     dtype=dtypes,
-#     nrows=80000
-# )
-#df_vitals = dd.from_pandas(df_sample, npartitions=1)
+#     nrows=80000)
+# print(df_sample.dtypes)
+# df_sample.to_csv("df_sample.csv", sep="|", index=False)
+# bad_mask = df_sample.apply(lambda col: col.astype(str).str.contains("2145-08-06", na=False))
+# bad_locs = bad_mask.any(axis=1)
+# print(df_sample.loc[bad_locs].head(5))
+# df_vitals = dd.from_pandas(df_sample, npartitions=1)
 df_vitals = dd.read_csv('/root/scripts/new_data/24hours/vitals_24_hours_final.csv', sep='|', dtype=dtypes)
 
 #df_vitals = dd.read_csv(r"C:\phd-final\phd\newapp\vitals_24_hours_final.csv", sep='|', dtype=dtypes)
